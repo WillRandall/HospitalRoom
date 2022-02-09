60443ef6499b6d522621aab341ab3f6bc977980c
Seth Hadley <22.seth.hadley@ksd.org>
2022-02-09 10:07:18 -0800

    SOUNDgit add -A!

---
+++ HospitalVR/Assets/Script/RandomSoundStart.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundStart : MonoBehaviour
+{
+    public AudioSource source;
+    public float minWaitBetweenPlays = 1f;
+    public float maxWaitBetweenPlays = 5f;
+    public float waitTimeCountdown = -1f;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            if (waitTimeCountdown < 0f)
+            {
+                source.Play();
+                waitTimeCountdown = Random.Range(minWaitBetweenPlays, maxWaitBetweenPlays);
+            }
+            else
+            {
+                waitTimeCountdown -= Time.deltaTime;
+            }
+        }
+    }
+}
