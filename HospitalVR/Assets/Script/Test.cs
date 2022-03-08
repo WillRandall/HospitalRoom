248770b933c93b0884aaa773c9feeaa495af1af5
Brandon Schaffer <22.brandon.schaffer@ksd.org>
2022-03-08 10:13:35 -0800

    added TEST script

--- HospitalVR/Assets/Script/Test.cs
+++ HospitalVR/Assets/Script/Test.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ink.Runtime;
 
-public class Test : MonoBehaviour
+public class TEST 
 {
-    // Start is called before the first frame update
-    void Start()
+  
+    public void StartListening(Story story)
     {
-        
+        story.variablesState.variableChangedEvent += VariableChanged;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void StopListening(Story story)
     {
-        
+        story.variablesState.variableChangedEvent -= VariableChanged;
     }
+
+    private void VariableChanged(string name, Ink.Runtime.Object value)
+    {
+        Debug.Log("Variable changed: " + name + " = " + value);
+    }
+
 }
