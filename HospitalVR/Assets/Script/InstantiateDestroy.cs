bf9003f7275a43ab2e446a4a3378a9d183f1b578
William Randall <22.william.randall@ksd.org>
2022-03-08 09:24:06 -0800

    ink.runtime fixed, added scriptending code that i think will work

--- HospitalVR/Assets/Script/InstantiateDestroy.cs
+++ HospitalVR/Assets/Script/InstantiateDestroy.cs
@@ -7,6 +7,7 @@ using Ink.Runtime;
 
 public class InstantiateDestroy : MonoBehaviour
 {
+    
     //script needs a more robust way of deciding when to delete and instantiate entities.
     int scriptEnding = 0;
     public GameObject EntityDel;
@@ -17,15 +18,15 @@ public class InstantiateDestroy : MonoBehaviour
     // am unable at this point to get "Story" or "_inkStory" to work
     public void UpdateFromStory()
     {
-        //commented next line out to avoid compiler errors
-        /* 
-         * scriptEnding = (int)story.variablesState["scriptEnding"];
-         */
+
+        
+          scriptEnding = (int)story.variablesState["scriptEnding"];
+         
         if (scriptEnding != 0)
         {
-            //destroy EntityDel
             Thread.Sleep(delay);
-            Instanstiate EntityInst;
+            Destroy(EntityDel);
+            Instantiate(EntityInst);
         }
     }
     
