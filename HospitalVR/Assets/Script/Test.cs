73eab86fc953995eec4dcba612e8a16efa722bd6
Brandon Schaffer <22.brandon.schaffer@ksd.org>
2022-03-08 10:26:31 -0800

    added to TEST script

--- HospitalVR/Assets/Script/Test.cs
+++ HospitalVR/Assets/Script/Test.cs
@@ -2,10 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ink.Runtime;
+using System.IO;
 
 public class TEST 
 {
-  
+
+    private Dictionary<string, Ink.Runtime.Object> variables;
+
+    public TEST(string globalsFilePath)
+    {
+        // Compile the story
+        string inkFileContents = File.ReadAllText(globalsFilePath);
+        Ink.Compiler compiler = new Ink.Compiler(inkFileContents);
+        Story globalsVariablesStory = compiler.Compile();
+
+        // initalize the dictionary 
+        variables = new Dictionary<string, Ink.Runtime.Object>();
+        foreach (string name in globalsVariablesStory.variablesState)
+        {
+            Ink.Runtime.Object value = globalsVariablesStory.variablesState.GetVariableWithName(name);
+            variables.Add(name, value);
+            Debug.Log("Initalized global dialouge variable: " + name + " = " + value);
+        }
+    }
+
     public void StartListening(Story story)
     {
         story.variablesState.variableChangedEvent += VariableChanged;
